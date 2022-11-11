00a245c6cd6e0f3720235e3e39a1ec288b545407
Abir <abiruzzaman.molla@gmail.com>
2022-11-11 17:48:17 +0600

    fix: route link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#"><h3>Simple CRUD</h3></a>
+          <a class="navbar-brand" href="{{ route('index') }}"><h3>Simple CRUD</h3></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
